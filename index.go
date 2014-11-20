575009843e4f555ca275311688feb7946cdc11bd
seunghyunjo <shj4517@naver.com>
2014-11-20 21:56:30 +0900

    11111

--- index.go
+++ index.go
@@ -3,16 +3,24 @@
 import (
     "net/http"
     "html/template"
-   
+
 )
  
-var mainTemplate, _ = template.ParseFiles("topics.html")
+
  
 func mainPage(w http.ResponseWriter, r *http.Request) {
-    mainTemplate.Execute(w, nil)
+	w.Header().Set("Content-type", "text/htm")
+  	mainTemplate:=template.Must(template.ParseFiles("topics.html"))
+	mainTemplate.Execute(w, nil)
 }
+
+
  
 func main() {
+			
     http.HandleFunc("/", mainPage)
     http.ListenAndServe(":8080", nil)
+
+	
 }
+
